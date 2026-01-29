df9fa73c8e9ff7c384d4e628b5e2ccc1f1f95ecd
g-genady-solomykin-gs <zeroprogramit@gmail.com>
2026-01-29 23:47:37 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,28 @@ async function setLang(lang) {
   const res = await fetch(`lang/${lang}.json`);
   const data = await res.json();
 
-//  document.querySelectorAll('[data-i18n]').forEach(el => {
-//   const key = el.getAttribute('data-i18n');
-//   if(data[key]) {
-//     if(el.tagName === 'IMG') {
-//       el.alt = data[key]; // для alt у картинок
-//     } else {
-//       el.textContent = data[key]; // для текста
-//     }
-//   }
-// });
+
 
 
 }
 
 setLang('ru');
+
+
+const videos = document.querySelectorAll('video[data-src]');
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+
+      const video = entry.target;
+      video.src = video.dataset.src;
+      video.load();
+
+      obs.unobserve(video);
+    });
+  }, {
+    rootMargin: '200px'
+  });
+
+  videos.forEach(video => observer.observe(video));
